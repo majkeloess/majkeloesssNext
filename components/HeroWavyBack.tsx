118808ede3bbd67b6a3d7e412d81f54d7c545a9c
majkeloess <msaturczak@student.agh.edu.pl>
2024-04-27 15:10:43 +0200

    new buttons

--- components/HeroWavyBack.tsx
+++ components/HeroWavyBack.tsx
@@ -10,7 +10,7 @@ import Image from "next/image";
 import animations from "@/lib/motion";
 export default function HeroBackground() {
   return (
-    <WavyBackground className="lg:pb-40 pb-32 flex flex-col items-center gap-10">
+    <WavyBackground className="lg:pb-40 pb-16 flex flex-col items-center gap-10">
       <div className="flex lg:flex-row flex-col-reverse gap-10 items-center">
         <div>
           <div className="flex flex-col gap-10 items-center">
@@ -26,7 +26,7 @@ export default function HeroBackground() {
                 {/* majke<span className="text-indigo-500 hidden xl:block">e</span> */}
                 majkeloe
                 <span className="text-purple-600">s</span>
-                <span className="text-blue-500">s</span>
+                <span className="text-indigo-600">s</span>
               </p>
               <p className="text-base md:text-lg lg:text-xl mt-4 text-white font-normal inter-var text-center">
                 Computer Science student with a passion for webDevelopment
@@ -52,7 +52,7 @@ export default function HeroBackground() {
           variants={animations.textVariant(0.5)}
           initial="hidden"
           whileInView="show"
-          className="h-[18rem] w-[18rem] rounded-full border-2 bg-cover bg-center border-blue-400"
+          className="h-[18rem] w-[18rem] rounded-full border-2 bg-cover bg-center border-purple-500"
           style={{
             backgroundImage: `url('https://github.com/majkeloess.png')`,
           }}
--- components/MainButton.tsx
+++ components/MainButton.tsx
@@ -3,12 +3,11 @@ import { motion } from "framer-motion";
 
 export default function MainButton({ text }: { text: string }) {
   return (
-    <motion.button
-      whileHover={{ scale: 1.1 }}
-      whileFocus={{ scale: 1.1 }}
-      className="px-8 py-2 rounded-2xl bg-gradient-to-tl from-blue-500 via-indigo-600 to-purple-700 text-white font-bold "
-    >
-      {text}
-    </motion.button>
+    <button className="p-[3px] relative">
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+      <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+        {text}
+      </div>
+    </button>
   );
 }
