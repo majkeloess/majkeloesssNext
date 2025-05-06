c2d76823e935b17e4dd8f31a82bfd052329b1339
majkeloess <msaturczak@student.agh.edu.pl>
2025-05-06 23:37:15 +0200

    refactor: remove unnecessary animations, make animations faster

--- lib/motion.ts
+++ lib/motion.ts
@@ -15,7 +15,6 @@ const animations = {
       transition: {
         type: "spring",
         stiffness: 80,
-        delay: 0.8,
       },
     },
   },
@@ -35,156 +34,11 @@ const animations = {
     show: {
       opacity: 1,
       transition: {
-        delayChildren: 1.2,
+        delayChildren: 0.4,
         staggerChildren: 0.1,
       },
     },
   },
-  parentListSecond: {
-    hidden: { opacity: 1 },
-    show: {
-      opacity: 1,
-      transition: {
-        delayChildren: 1.8,
-        staggerChildren: 0.1,
-      },
-    },
-  },
+}
 
-  slideIn: (
-    direction: string,
-    type: string,
-    delay: number,
-    duration: number
-  ) => ({
-    hidden: {
-      x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
-      y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
-    },
-    show: {
-      x: 0,
-      y: 0,
-      transition: {
-        type,
-        delay,
-        duration,
-        ease: "easeOut",
-      },
-    },
-  }),
-
-  staggerContainer: (staggerChildren: number, delayChildren: number) => ({
-    hidden: {},
-    show: {
-      transition: {
-        staggerChildren,
-        delayChildren,
-      },
-    },
-  }),
-
-  textVariant: (delay: number) => ({
-    hidden: {
-      y: 50,
-      opacity: 0,
-    },
-    show: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        type: "spring",
-        duration: 1.2,
-        delay,
-      },
-    },
-  }),
-
-  textContainer: {
-    hidden: {
-      opacity: 0,
-    },
-    show: (i = 1) => ({
-      opacity: 1,
-      transition: { staggerChildren: 0.1, delayChildren: i * 0.1 },
-    }),
-  },
-
-  textVariant2: {
-    hidden: {
-      opacity: 0,
-      y: 20,
-    },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        type: "tween",
-        ease: "easeIn",
-      },
-    },
-  },
-
-  fadeIn: (
-    direction: string,
-    type: string,
-    delay: number,
-    duration: number
-  ) => ({
-    hidden: {
-      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-      opacity: 0,
-    },
-    show: {
-      x: 0,
-      y: 0,
-      opacity: 1,
-      transition: {
-        type,
-        delay,
-        duration,
-        ease: "easeOut",
-      },
-    },
-  }),
-
-  zoomIn: (delay: number, duration: number) => ({
-    hidden: {
-      scale: 0,
-      opacity: 0,
-    },
-    show: {
-      scale: 1,
-      opacity: 1,
-      transition: {
-        type: "tween",
-        delay,
-        duration,
-        ease: "easeOut",
-      },
-    },
-  }),
-
-  footerVariants: {
-    hidden: {
-      opacity: 0,
-      y: 50,
-      transition: {
-        type: "spring",
-        stiffness: 300,
-        damping: 140,
-      },
-    },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        type: "spring",
-        stiffness: 80,
-        delay: 0.5,
-      },
-    },
-  },
-};
-
-export default animations;
+export default animations
